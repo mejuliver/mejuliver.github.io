f1d833d81ee8238f347017c63c2451e2158e58e8
Juliver Galleto <juliver.galleto@gmail.com>
2023-04-14 10:57:11 +0800

    added python on work

--- src/src/components/Works.svelte
+++ src/src/components/Works.svelte
@@ -23,7 +23,7 @@
 			img: 'img/rfid-qr-scanner.jpg',
 			url: '',
 			type: 'POS',
-			frameworks: ['Electron','Node','Vue','Lumen','Socketio','Raspberry pi'],
+			frameworks: ['Electron','Node','Vue','Lumen','Socketio','Raspberry pi','Python'],
 			works: 'RFID and QR scanner, attendance system',
 			active: true
 		},
